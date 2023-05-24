554a1252d0e85780f538c9fbfc9d75cf3b0ce3db
Vipin kumar <vipinkumarvipinkumar20@gmail.com>
2023-05-24 13:20:22 +0530

    Added Tourist Table

---
+++ TouristWorldDb/Tbl_Touristworld.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tbl_Touristworld]
+(
+		[PlaceId] INT NOT NULL PRIMARY KEY,
+	LocationName varchar(100),
+	City varchar (150),
+	Country varchar(100),
+	LocationImage varchar(max)
+)
